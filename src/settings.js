4222765a2cd45f8d988ff0d0df82f547d87a023f
hazembz <hazembenbz@gmail.com>
2025-08-22 13:47:04 +0100

    chore: Create settings file

---
+++ src/settings.js
@@ -0,0 +1,5 @@
+export const projectName = "Well Lifecycle SDK";
+
+// env var expansion with .env files is failing -> just move to vite later
+const targetGateway = "localhost:8000";
+export const targetServer = targetGateway;
